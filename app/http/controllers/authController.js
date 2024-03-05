1c50de1c2e0b203f127eadb486f509a3a7af4066
Yash Jhalani <97944350+yash72310@users.noreply.github.com>
2024-03-05 16:06:23 +0530

    Update authController.js

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -5,6 +5,8 @@ function authController() {
     const _getRedirectUrl = (req) => {
         return req.user.role === 'admin' ? '/admin/orders' : '/#all_food'
     }
+
+    
     
     return {
         login(req, res) {
@@ -26,6 +28,10 @@ function authController() {
                     req.flash('error', info.message )
                     return res.redirect('/login')
                 }
+                if (!user.verified) {
+                   req.flash('error',"Email is not verified! Please verify the email")
+                   return res.redirect('/login')
+                  }
                 req.logIn(user, (err) => {
                     if(err) {
                         req.flash('error', info.message ) 
@@ -42,50 +48,20 @@ function authController() {
             res.render('auth/register')
         },
 
-        
-        async postRegister(req, res) {
-         const { name, email, role , password }   = req.body
-         // Validate request 
-         if(!name || !email || !password) {
-             req.flash('error', 'All fields are required')
-             req.flash('name', name)
-             req.flash('email', email)
-            return res.redirect('/register')
-         }
-
-         // Check if email exists 
-         User.exists({ email: email }, (err, result) => {
-             if(result) {
-                req.flash('error', 'Email already taken')
-                req.flash('name', name)
-                req.flash('email', email) 
-                return res.redirect('/register')
-             }
-         })
-
-         // Hash password 
-         const hashedPassword = await bcrypt.hash(password, 10)
-         // Create a user 
-         const user = new User({
-             name,
-             email,
-             role,
-             password: hashedPassword
-         })
-
-         user.save().then((user) => {
-            // Login
-            return res.redirect('/login')
-         }).catch(err => {
-            req.flash('error', 'Something went wrong')
-                return res.redirect('/register')
-         })
-        },
-        logout(req, res) {
-          req.logout()
+        logout(req, res){
+          req.logout();
           return res.redirect('/login')  
+        },
+
+
+       
         }
+
     }
-}
+
+
+
+
+
 
 module.exports = authController
