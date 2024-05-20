e6adfbe19226d4099b001d034f6197feb7afc224
Yash Jhalani <97944350+yash72310@users.noreply.github.com>
2024-05-20 22:28:54 +0530

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,6 @@ const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String },
     verified:{type:Boolean,default:false},
 }, { timestamps: true })
 
